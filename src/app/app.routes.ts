b2b5a3a07643481da09d24b3cf4bf92d21452a7f
Victor Salgado <salgadovictor91@gmail.com>
2026-05-02 23:23:28 -0600

    se trabaja en el componente Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { PublicLayout } from './layouts/public-layout/public-layout';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
 {
 path: '',
 component: PublicLayout,
 children: [
-
+{ path: '', component: Home },
 
 
 ]}];
